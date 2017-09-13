f727dee4d46b093791901ff6ec2449b6c3399cae
mildis <me@mildis.org>
2017-09-13 18:31:16 +0200

    Use multiple providers to confirm the public IP

--- gandi-update.sh
+++ gandi-update.sh
@@ -6,23 +6,37 @@ DOMAIN=example.com
 HOST=my-server
 TTL=86400
 RETRY=5
+INDEX=0
 
-while [ x"${CURRENT_IPV4}" = "x" -a ${RETRY} -gt 0 ]; do
-	((RETRY--))
-	CURRENT_IPV4=$(curl -qs -m 30 http://ifconfig.me/ip)
+PUBLIC_IPV4_PROVIDERS=(http://ifconfig.me/ip https://ipv4.lafibre.info/ip.php)
+PUBLIC_IPV4=
+
+for PUBPROV in ${PUBLIC_IPV4_PROVIDERS[@]}; do
+	while [ x"${PUBLIC_IPV4[${INDEX}]}" = "x" -a ${RETRY} -gt 0 ]; do
+		((RETRY--))
+		PUBLIC_IPV4[${INDEX}]=$(curl -qs -m 30 ${PUBPROV})
+	done
+	((INDEX++))
 done
+
+CURRENT_IPV4=$(echo "${PUBLIC_IPV4[@]}"|tr ' ' '\n'|sort -u)
+
+if [ $(echo "${CURRENT_IPV4}"|wc -l) -ne 1 ]; then
+	echo "Not all IP found are the same :\n${CURRENT_IPV4}" >&2
+	exit 1
+fi
 
 GANDI_DNS=$(gandi record list -f json ${DOMAIN}|jq -r '.[]|select(.name == '\"${HOST}\"')|[.name, .ttl, .type, .value]|@tsv'|tr '\t' ' ')
 GANDI_IPV4=$(echo ${GANDI_DNS}|cut -d' ' -f4)
 
 if [ -z "${CURRENT_IPV4}" ]; then
 	echo "ERROR : cannot find current IPv4" >&2
-	exit 1
+	exit 2
 fi
 
 if [ -z "${GANDI_DNS}" -o -z "${GANDI_IPV4}" ]; then
 	echo "ERROR : cannot get Gandi informations" >&2
-	exit 2
+	exit 3
 fi
 
 if [ x"${CURRENT_IPV4}" = x"${GANDI_IPV4}" ]; then
